9354b8bea70185d78ec7c514545a5494ac284c1c
Ronak H Shah <rons.shah@gmail.com>
2016-08-10 13:38:35 -0400

    Bug Fixes

--- run_process-pusedogene_analysis.py
+++ run_process-pusedogene_analysis.py
@@ -166,10 +166,14 @@ def SetupRun(poolName, id, args):
         if(args.verbose):
             print "\tQC LOCATION", qclocation, " DOES NOT EXISTS!!!, Please Review you qcLocation INPUT\n"
     sv_dir = qclocation + "/StrVarAnalysis/" + id
-    delFile = glob.glob(sv_dir + "/*del.vcf")[0]
-    dupFile = glob.glob(sv_dir + "/*dup.vcf")[0]
-    invFile = glob.glob(sv_dir + "/*inv.vcf")[0]
-    jmpFile = glob.glob(sv_dir + "/*jmp.vcf")[0]     
+    delFile = glob.glob(sv_dir + "/*del.vcf")
+    delFile = delFile[0]
+    dupFile = glob.glob(sv_dir + "/*dup.vcf")
+    dupFile = dupFile[0]
+    invFile = glob.glob(sv_dir + "/*inv.vcf")
+    invFile = invFile[0]
+    jmpFile = glob.glob(sv_dir + "/*jmp.vcf")  
+    jmpFile = jmpFile[0]   
     return(delFile)
 
 
