60860495d17b1c8598368ca541b310c6b43077a3
Ronak H Shah <rons.shah@gmail.com>
2016-12-14 10:17:05 -0500

    Remove Duplicate Rows

--- get_DMP_ID.py
+++ get_DMP_ID.py
@@ -174,6 +174,7 @@ def main():
             
     
     outDF.sort_values(["GROUP_ID"], inplace=True, ascending=True)  
+    outDF.drop_duplicates(inplace=True)
     outDF.to_csv(args.outFile, sep='\t', index=False)
     if(verbose):
         logger.info("get_DMP_ID: Finished Mapping, Final data written in %s", args.outFile)
